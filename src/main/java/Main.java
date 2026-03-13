d653f5bcb6442f9e3372c440b4e120dd1acdf24a
PHGil <PHGil@users.noreply.github.com>
2026-03-13 23:06:30 +0000

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,12 +12,10 @@ void main() throws Exception {
             System.out.println(input.substring(echoCommand.length()));
         } else if (input.startsWith(typeCommand)) {
             final String typeInput = input.substring(typeCommand.length());
-            if (builtinCommands.contains(typeInput)) {
-                System.out.println(typeInput + " is a shell builtin");
+            if (!builtinCommands.contains(typeInput)) {
+                System.out.println(input + ": command not found");
             }
-        } else {
-            System.out.println(input + ": command not found");
+            System.out.println(typeInput + " is a shell builtin");
         }
-
     }
 }
