a2a31666a8b1c863d69020a63ee5da0c7ad2947d
PHGil <PHGil@users.noreply.github.com>
2026-03-13 22:49:50 +0000

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
 void main() throws Exception {
     Scanner scanner = new Scanner(System.in);
+    final String echoInput = "echo ";
     while (true) {
         System.out.print("$ ");
         String input = scanner.nextLine();
         if (input.equals("exit")) {
             break;
         }
-        if (input.startsWith("echo ")) {
-            System.out.println(input);
+        if (input.startsWith(echoInput)) {
+            System.out.println(input.substring(echoInput.length()));
         }
         System.out.println(input + ": input not found");
     }
