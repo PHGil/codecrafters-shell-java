5231088d0ec45c08382dda62e044d8b7aeb49388
PHGil <PHGil@users.noreply.github.com>
2026-03-13 22:28:37 +0000

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
-    }
+void main() throws Exception {
+    System.out.print("$ ");
+    Scanner scanner = new Scanner(System.in);
+    String command = scanner.nextLine();
+    System.out.println(command + ": command not found" );
 }
