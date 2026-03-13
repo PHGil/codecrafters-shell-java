b01621efab6f7997570261a269935514b48f2b31
PHGil <PHGil@users.noreply.github.com>
2026-03-13 22:41:52 +0000

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
-
 void main() throws Exception {
-    System.out.print("$ ");
-    Scanner scanner = new Scanner(System.in);
-    String command = scanner.nextLine();
-    System.out.println(command + ": command not found" );
+    while (true) {
+        System.out.print("$ ");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        System.out.println(command + ": command not found");
+    }
 }
