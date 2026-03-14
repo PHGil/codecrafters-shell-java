eea9f95d1fd377e42365353a9a43f4b5bb70c3c7
PHGil <PHGil@users.noreply.github.com>
2026-03-14 00:01:25 +0000

    codecrafters submit [skip ci]

--- src/main/java/command/Command.java
+++ src/main/java/command/Command.java
@@ -3,7 +3,7 @@ package command;
 public interface Command {
     default void initArgs(String[] args) {
         if (args[1] != null && !args[1].isEmpty()) {
-            args[1].trim();
+            args[1] = args[1].trim();
         }
     }
 
