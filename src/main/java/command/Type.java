2466a12281ae5f3d3ba3ea939327ed4c144471f0
PHGil <PHGil@users.noreply.github.com>
2026-03-14 15:48:56 +0000

    codecrafters submit [skip ci]

--- src/main/java/command/Type.java
+++ src/main/java/command/Type.java
@@ -1,16 +1,32 @@
 package command;
 
+import java.io.File;
+
 public class Type implements Command {
-    public void execute(String[] args) {
+    private final static String PATH = System.getenv("PATH");
+    private final static String DELIMETER = File.pathSeparator;
+
+    public void execute(final String[] args) {
         if (args.length < 2) {
             System.out.println("type: missing operand");
             return;
         }
-        String name = args[1];
-        if (!CommandsEnum.contains(name)) {
-            System.out.println(name + " not found");
+        final String command = args[1];
+        if (!CommandsEnum.contains(command)) {
+            System.out.println(checkCommandPath(command));
         } else {
-            System.out.println(name + " is a shell builtin");
+            System.out.println(command + " is a shell builtin");
         }
+    }
+
+    private String checkCommandPath(final String command) {
+        final String[] paths = PATH.split(DELIMETER);
+        for (String path : paths) {
+            final File file = new File(path, command);
+            if (file.exists() && file.canExecute()) {
+                return command + " is " + file.getAbsolutePath();
+            }
+        }
+        return command + " not found";
     }
 }
