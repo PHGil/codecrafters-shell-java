d45b658f474795e4593836691d2009caa2325ce9
PHGil <PHGil@users.noreply.github.com>
2026-03-15 22:46:00 +0000

    codecrafters submit [skip ci]

--- src/main/java/command/CD.java
+++ src/main/java/command/CD.java
@@ -1,18 +1,47 @@
 package command;
 
 public class CD implements Command {
+    private static final String USER_DIR = "user.dir";
+    private static final String HOME = "~";
+    private static final String HOME_DIR = System.getProperty("user.home");
+
     @Override
     public String execute(String[] args) {
         if (args.length > 1) {
             return "cd: too many arguments";
         }
-        String targetDir = args.length == 0
-                ? System.getProperty("user.home")
-                : args[0];
+        if (args.length == 0 || this.isHomeDirectory(args[0])) {
+            System.setProperty(USER_DIR, HOME_DIR);
+        }
+        String targetDir = args[0];
         if (!Utils.isDirectoryPresent(targetDir)) {
             return "cd: " + targetDir + ": No such file or directory";
         }
-        System.setProperty("user.dir", targetDir);
+        if (this.isCurrentDirectory(targetDir)) {
+            targetDir = System.getProperty(USER_DIR) + "/" + targetDir;
+        }
+        if (this.isParentDirectory(targetDir)) {
+            targetDir = this.handleParentDirectory(targetDir);
+        }
+        System.setProperty(USER_DIR, targetDir);
         return "";
+    }
+
+    private String handleParentDirectory(final String path) {
+        String currentDir = System.getProperty(USER_DIR);
+        String parentDir = currentDir.substring(0, currentDir.lastIndexOf('/'));
+        return parentDir + path.substring(2);
+    }
+
+    private boolean isCurrentDirectory(final String path) {
+        return path.startsWith("./") || !path.matches("^[^/]*");
+    }
+
+    private boolean isHomeDirectory(final String path) {
+        return path.startsWith(HOME);
+    }
+
+    private boolean isParentDirectory(final String path) {
+        return path.startsWith("../");
     }
 }
