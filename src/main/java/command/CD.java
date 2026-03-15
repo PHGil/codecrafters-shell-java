567f2d5f1a44764e66822a0dcde347fb22be24e1
PHGil <PHGil@users.noreply.github.com>
2026-03-15 19:20:15 +0000

    codecrafters submit [skip ci]

--- src/main/java/command/CD.java
+++ src/main/java/command/CD.java
@@ -3,12 +3,14 @@ package command;
 public class CD implements Command {
     @Override
     public String execute(String[] args) {
+        System.out.println(System.getProperty("user.dir"));
         if (args.length > 1) {
             return "cd: too many arguments";
         }
         String targetDir = args.length == 0
                 ? System.getProperty("user.home")
                 : args[0];
+        System.out.println(targetDir);
         if (args.length == 1 && !Utils.isDirectoryPresent(targetDir)) {
             return "cd: " + targetDir + ": No such file or directory";
         }
