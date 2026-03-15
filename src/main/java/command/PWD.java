343cc79611aa53f442defdef970c86421ddc4d90
PHGil <PHGil@users.noreply.github.com>
2026-03-15 19:34:33 +0000

    codecrafters submit [skip ci]

--- src/main/java/command/PWD.java
+++ src/main/java/command/PWD.java
@@ -3,6 +3,6 @@ package command;
 public class PWD implements Command {
     @Override
     public String execute(String[] args) {
-        return System.getenv("PWD");
+        return System.getProperty("user.dir");
     }
 }
