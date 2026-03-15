a6fcd996a59519ab9e599bc510e1668ac59c039f
PHGil <PHGil@users.noreply.github.com>
2026-03-15 22:49:59 +0000

    codecrafters submit [skip ci]

--- src/main/java/command/CD.java
+++ src/main/java/command/CD.java
@@ -1,5 +1,7 @@
 package command;
 
+import java.nio.file.Paths;
+
 public class CD implements Command {
     private static final String USER_DIR = "user.dir";
     private static final String HOME = "~";
@@ -12,25 +14,15 @@ public class CD implements Command {
         }
         if (args.length == 0) {
             System.setProperty(USER_DIR, HOME_DIR);
-            return "";  // Must return here to avoid ArrayIndexOutOfBoundsException
+            return "";
         }
-
         String targetDir = args[0];
-
-        // Handle ~ (home directory)
         if (this.isHomeDirectory(targetDir)) {
             targetDir = targetDir.replaceFirst("~", HOME_DIR);
-        }
-        // Handle .. (parent directory)
-        else if (this.isParentDirectory(targetDir)) {
-            targetDir = this.handleParentDirectory(targetDir);
-        }
-        // Handle relative paths (don't start with /)
-        else if (this.isRelativePath(targetDir)) {
+        } else if (this.isRelativePath(targetDir)) {
             targetDir = System.getProperty(USER_DIR) + "/" + targetDir;
         }
-        // Absolute paths (start with /) - no modification needed
-
+        targetDir = Paths.get(targetDir).normalize().toString();
         if (!Utils.isDirectoryPresent(targetDir)) {
             return "cd: " + targetDir + ": No such file or directory";
         }
@@ -39,21 +31,11 @@ public class CD implements Command {
         return "";
     }
 
-    private String handleParentDirectory(final String path) {
-        String currentDir = System.getProperty(USER_DIR);
-        String parentDir = currentDir.substring(0, currentDir.lastIndexOf('/'));
-        return parentDir + path.substring(2);
-    }
-
     private boolean isRelativePath(final String path) {
-        return !path.startsWith("/");  // Simple and correct
+        return !path.startsWith("/");
     }
 
     private boolean isHomeDirectory(final String path) {
         return path.startsWith(HOME);
-    }
-
-    private boolean isParentDirectory(final String path) {
-        return path.startsWith("..");
     }
 }
